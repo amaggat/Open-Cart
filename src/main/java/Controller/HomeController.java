1b7551d16d5df0455587122f9c06dc3a90c17675
thanhdat9620 <thanhdat.9620@gmail.com>
2020-07-17 14:41:01 +0700

    thanhdat

---
+++ src/main/java/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping(value={"/", "/home"})
+    public String home(){
+        return "home";
+    }
+}
