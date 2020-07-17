2749266e1142159d592898636a02f55df514953e
thanhdat9620 <thanhdat.9620@gmail.com>
2020-07-17 14:09:20 +0700

    thahdat

--- src/main/java/Model/Order.java
+++ src/main/java/Model/Order.java
@@ -1,4 +1,21 @@
 package Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+    private int orderID;
+    private int customerID;
+    private int productID;
+    private String status;
+    private Date dateReceived;
+    private Date dateDelivered;
 }
--- src/main/java/Model/OrderDetail.java
+++ src/main/java/Model/OrderDetail.java
@@ -1,7 +1,19 @@
 package Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDetail {
     private int orderID;
     private String description;
-    private
+    private int quantity;
+    private Date dateRequired;
 }
