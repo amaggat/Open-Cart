40c24bc98eed2b4fd1cc6d1064fa21fd0b7b1eb2
ducduongn <48018619+ducduongn@users.noreply.github.com>
2020-07-23 02:04:43 +0700

    Add Security

---
+++ src/main/java/opencart/OpenCartApp.java
@@ -0,0 +1,14 @@
+package opencart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class OpenCartApp {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenCartApp.class, args);
+    }
+}
