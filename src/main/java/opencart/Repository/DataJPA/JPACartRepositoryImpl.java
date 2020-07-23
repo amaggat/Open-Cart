59c85470584d0184fc4410b6c2dc4f8ad5f4da5f
thanhdat9620 <thanhdat.9620@gmail.com>
2020-07-23 19:29:42 +0700

    WishList and Cart JPA Query

--- src/main/java/opencart/Repository/DataJPA/JPACartRepositoryImpl.java
+++ src/main/java/opencart/Repository/DataJPA/JPACartRepositoryImpl.java
@@ -4,46 +4,86 @@ import opencart.Model.Cart;
 import opencart.Model.Product;
 import opencart.Model.WishList;
 import opencart.Repository.CartRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.Collection;
 
+@Repository
 public class JPACartRepositoryImpl implements CartRepository {
     @PersistenceContext
     private EntityManager em;
 
     @Override
     @SuppressWarnings("unchecked")
+    @Transactional
     public Collection<Product> findAllProduct() {
-        Query query = this.em.createQuery("SELECT productName FROM product p INNER JOIN cart-product cp ON cp.productId = p.productId INNER JOIN cart c ON c.customerId = cp.customerId");
+        Query query = this.em.createQuery(
+                "SELECT productName FROM product p " +
+                        "INNER JOIN cart-product cp ON cp.productId = p.productId " +
+                        "INNER JOIN cart c ON c.customerId = cp.customerId");
+        Collection<Product> products = query.getResultList();
+        return products;
+    }
+
+    @Override
+    @Transactional
+    public Collection<Product> addToWishList(Product product) {
+        this.em.getTransaction().begin();
+        this.em.persist(product);
+        this.em.getTransaction().commit();
+        Query query = this.em.createQuery(
+                "SELECT * FROM product p " +
+                        "INNER JOIN wishlist-product wp ON wp.productID = p.productID\n");
         return query.getResultList();
     }
 
     @Override
-    public void addToWishList(Product product) {
-        WishList wishlist = em.find(WishList.class, 1);
-        em.getTransaction().begin();
-        em.merge(product);
-        em.getTransaction().commit();
+    @Transactional
+    public Collection<Product> removeProductInCart(Product product) {
+        //DELETE FROM cart-product cp WHERE cp.productID = productID
+        Query query = this.em.createQuery("DELETE FROM cart-product cp WHERE cp.productID = :productID", Product.class);
+        Collection<Product> products = query.setParameter("productID", product.getId()).getResultList();
+        return products;
     }
 
     @Override
-    public void removeProductInCart(Product product) {
-        this.em.getTransaction().begin();
-        this.em.remove(product);
-        this.em.getTransaction().commit();
+    @Transactional
+    public Collection<Product> addProduct(Integer productID, Integer customerID) {
+        Query query = this.em.createQuery("INSERT INTO cart-product(customerID, productID) VALUES (:customerId, :productID)");
+        query.setParameter("productID", productID).setParameter("customerID", customerID).getResultList();
+        Query query1 = this.em.createQuery(
+                "SELECT productName FROM product p " +
+                        "INNER JOIN cart-product cp ON cp.productID = p.productID");
+        Collection<Product> products = query1.getResultList();
+        return products;
     }
 
     @Override
-    public void addProduct(Product product) {
-        this.em.getTransaction().begin();
-        this.em.merge(product);
-        this.em.getTransaction().commit();
+    @Transactional
+    public String findCartByID(Integer ID) {
+        Query query = this.em.createQuery("SELECT customerName cus FROM customer c" +
+                "INNER JOIN cart c ON c.customerID = cus.customerID" +
+                "WHERE cus.customerID = :ID");
+        String name = (String) query.setParameter("ID", ID).getSingleResult();
+        return name;
     }
 
     @Override
+    @Transactional
+    public Collection<Cart> findCartByName(String name) {
+        Query query = this.em.createQuery("SELECT customerName cus FROM customer c" +
+                "INNER JOIN cart c ON c.customerID = cus.customerID" +
+                "WHERE cus.customerName = :customerName");
+        Collection<Cart> carts = query.setParameter("customerName", name).getResultList();
+        return carts;
+    }
+
+    @Override
+    @Transactional
     public void save(Cart cart) {
         if (cart.getId() == null) this.em.persist(cart);
         else this.em.merge(cart);
--- src/main/java/opencart/Repository/DataJPA/JPAWishListRepositoryImpl.java
+++ src/main/java/opencart/Repository/DataJPA/JPAWishListRepositoryImpl.java
@@ -4,16 +4,19 @@ import opencart.Model.Cart;
 import opencart.Model.Product;
 import opencart.Model.WishList;
 import opencart.Repository.WishListRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.util.Collection;
 import java.util.List;
 
+@Repository
 public class JPAWishListRepositoryImpl implements WishListRepository {
 
     @PersistenceContext
-    EntityManager em;
+    private EntityManager em;
 
     @Override
     public List<Product> findAllProductInWishList() {
@@ -22,18 +25,29 @@ public class JPAWishListRepositoryImpl implements WishListRepository {
     }
 
     @Override
-    public void addToCart(Product product) {
-        Cart cart = em.find(Cart.class, 1);
-        em.getTransaction().begin();
-        em.merge(product);
-        em.getTransaction().commit();
+    public Collection<Product> addToCart(Integer productID, Integer customerID) {
+        Query query = this.em.createQuery("INSERT INTO cart-product(customerID, productID) VALUES(:customerID, :productID)");
+        query.setParameter("customerID", customerID).setParameter("productID", productID);
+        Collection<Product> products = query.getResultList();
+        return products;
     }
 
     @Override
-    public void removeProductInWishList(Product product) {
-        em.getTransaction().begin();
-        em.remove(product);
-        em.getTransaction().commit();
+    public Collection<Product> removeProductInWishList(Product product) {
+        Query query = this.em.createQuery("DELETE FROM wishlist-product wp WHERE wp.productID = :productID");
+        query.setParameter("productID", product.getId());
+        Collection<Product> products = query.getResultList();
+        return null;
+    }
+
+    @Override
+    public Collection<Product> addProduct(Integer productId, Integer customerID) {
+        Query query = this.em.createQuery("\n" +
+                "INSERT INTO wishlist-product(customerID, productID) VALUES(:customerID, :productID)");
+        query.setParameter("productID", productId).setParameter("customerID", customerID);
+        Query query1 = this.em.createQuery("SELECT productName FROM product p " +
+                "INNER JOIN wishlist-product wp ON wp.productID = p.productID");
+        return query1.getResultList();
     }
 
     @Override
@@ -41,4 +55,5 @@ public class JPAWishListRepositoryImpl implements WishListRepository {
         if(wishList.getId()==null) this.em.merge(wishList);
         else this.em.persist(wishList);
     }
+
 }
