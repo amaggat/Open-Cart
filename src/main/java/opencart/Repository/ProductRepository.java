a8647c7d0cf2f01825d7a7f676998a92b8228637
NguyenVanQuangk63uetCACLC1 <50706070+NguyenVanQuangk63uetCACLC1@users.noreply.github.com>
2020-07-26 23:51:32 +0700

    26/7/20

--- src/main/java/opencart/Repository/ProductRepository.java
+++ src/main/java/opencart/Repository/ProductRepository.java
@@ -1,15 +1,24 @@
+/*
+25/7/20
+ */
 package opencart.Repository;
 
 import opencart.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 import java.util.Collection;
-
-public interface ProductRepository {
-    Collection<Product> findAllProduct();
+@EnableJpaRepositories
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    //    Collection<Product> findAllProduct();
     Collection<Product> findByName(String name);
     Collection<Product> findByBrandName();
     Product findByID(int ID);
-    void save(Product product);
+//    void save(Product product);
+//    void delete(Integer ID);
+
+
     Collection<Product> addToCart (int productID, int cartID);
     Collection<Product> addToWishList(int productID, int wishListID);
 }
