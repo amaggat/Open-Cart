99ad970a5a5b83d1d80108b159117dee4a15ad3e
NguyenVanQuangk63uetCACLC1 <50706070+NguyenVanQuangk63uetCACLC1@users.noreply.github.com>
2020-07-30 23:33:56 +0700

    customer mvc

--- src/main/java/opencart/Controller/CustomerController.java
+++ src/main/java/opencart/Controller/CustomerController.java
@@ -1,13 +1,47 @@
 package opencart.Controller;
 
+import opencart.Model.Customer;
+import opencart.Model.Product;
+import opencart.Service.ServiceInt.CustomerService;
 import opencart.Service.ServiceInt.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collection;
 
 @Controller
 public class CustomerController {
 
     @Autowired
-    private ProductService productService;
+    private CustomerService customerService;
+
+    @RequestMapping("/customer")
+    public String showCustomer(Model model)
+    {
+        Collection<Customer> listCustomer = customerService.showAllCustomer();
+        model.addAttribute("listCustomer",listCustomer);
+        return "customer";
+    }
+    @RequestMapping("/customer?")
+    public ModelAndView editProduct(@PathVariable("id") Integer id)
+    {
+        ModelAndView modelAndView = new ModelAndView("access");
+        Customer customer = customerService.findCustomerByID(id);
+        modelAndView.addObject("customer",customer);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/confirm", method = RequestMethod.POST)
+    public String checkCustomer(@ModelAttribute("customer") Customer customer) {
+        customerService.findCustomerByAccountAndPassword(customer.getAccountName(),customer.getPassword());
+        return "customerinfo";
+    }
+
 
 }
